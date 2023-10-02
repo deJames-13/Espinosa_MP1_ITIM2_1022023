766e642fcf7a831fd8ed3f3470b78ef8993b158f
James Espinosa <de.james013@gmail.com>
2023-10-02 21:45:39 +0800

    Espinosa MP1

--- update.php
+++ update.php
@@ -1,21 +1,25 @@
 <?php
-    include('./config.php');
-    $id = $_POST['id'];
-    $name=$_POST['name'];
-    $album=$_POST['album'];
-    $genre=$_POST['genre'];
-    $artist=$_POST['artist'];
-    $year=$_POST['year'];
-
-    $query = "UPDATE songs SET `name`='$name', album='$album', genre='$genre', artist='$artist', `year`='$year' WHERE id='$id'";
-    $result = mysqli_query($conn, $query);
+include('./config.php');
+$check = empty($_POST['name']) || empty($_POST['album']) || empty($_POST['genre']) || empty($_POST['artist']) || empty($_POST['year']);
 
 
-    if ($result > 0) {
-        header("Location: index.php");
-    }
-    else {
-        echo "Error while updating song..";
-    }
+if ($check) {
+    header("Location: index.php");
+    return;
+}
+$id = $_POST['id'];
+$name = $_POST['name'];
+$album = $_POST['album'];
+$genre = $_POST['genre'];
+$artist = $_POST['artist'];
+$year = $_POST['year'];
 
-?>
+$query = "UPDATE songs SET `name`='$name', album='$album', genre='$genre', artist='$artist', `year`='$year' WHERE id='$id'";
+$result = mysqli_query($conn, $query);
+
+
+if ($result > 0) {
+    header("Location: index.php");
+} else {
+    echo "Error while updating song..";
+}
